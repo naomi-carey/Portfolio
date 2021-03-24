431754af3b8b9670672958ace552f612eca2e946
Naomi Carey <naomicarey00@gmail.com>
2021-03-24 10:52:27 +0100

    added global Infosection and assoicated images

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,18 @@
+export const homeObjOne = {
+  id: "about",
+  lightBg: false,
+  lightTextDesc: true,
+  topLine: "Premium Bank",
+  healdine: "Unlimited Transactions with zero fees",
+  description:
+    "Get access to our exclusive app that allows you to send unlimited transactions",
+  buttonLabel: "Get Started",
+  imgStart: false,
+  img: require("../../images/svg-1.svg"),
+  alt: "Car",
+  dark: "true",
+  primary: true,
+  darkText: false,
+  primary: true,
+  darkText: false,
+};
