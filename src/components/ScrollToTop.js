ac45c90774a2a464464bf77f43cf67e2903a7bbc
Naomi Carey <naomicarey00@gmail.com>
2021-05-09 10:48:51 +0200

    testing scrolltotop in about component

--- src/components/ScrollToTop.js
+++ src/components/ScrollToTop.js
@@ -1,11 +1,24 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
-export default function ScrollToTop() {
-  const { pathname } = useLocation();
+// export default function ScrollToTop() {
+//   const { pathname } = useLocation();
 
+//   useEffect(() => {
+//     window.scrollTo(0, 0);
+//   }, [pathname]);
+//   return null;
+// }
+
+const ScrollTop = () => {
+  const { pathname } = useLocation();
   useEffect(() => {
-    window.scrollTo(0, 0);
+    window.scroll({
+      top: 0,
+      left: 0,
+    });
   }, [pathname]);
   return null;
-}
+};
+
+export default ScrollTop;
